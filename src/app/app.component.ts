92c2c1619d18e0c4c8aad3e9f87cd2216b8bf04e
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-17 13:41:29 +0200

    componente home para ruta vacía

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,37 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  public proyectos = [
-    {
-      name: 'Aprender angular',
-      status: 1,
-    },
-    {
-      name: 'Tener un buen trabajo',
-      status: 0,
-    },
-  ];
-  public color = 'brown';
-
-  public elValor = 5;
-
-  // private paella = 55;
-  // public get elValor(): number {
-  //   return this.paella;
-  // }
-  // public set elValor(arg: number) {
-  //   this.paella = arg;
-  // }
-
-  public multiplo(cantidad: number): number {
-    return cantidad * this.elValor;
-  }
-
-  public incrementar(): void {
-    this.elValor++;
-  }
-  reducir(): void {
-    this.elValor--;
-  }
-}
+export class AppComponent {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  public proyectos = [
+    {
+      name: 'Aprender angular',
+      status: 1,
+    },
+    {
+      name: 'Tener un buen trabajo',
+      status: 0,
+    },
+  ];
+  public color = 'brown';
+
+  public elValor = 5;
+
+  public multiplo(cantidad: number): number {
+    return cantidad * this.elValor;
+  }
+
+  public incrementar(): void {
+    this.elValor++;
+  }
+  reducir(): void {
+    this.elValor--;
+  }
+}
