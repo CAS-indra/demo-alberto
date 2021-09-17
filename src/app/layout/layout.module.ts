ac41ed4883688f0cbc4f0641923882f842f9edae
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-17 13:58:20 +0200

    routerLink

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { DesarrolladorComponent } from './desarrollador.component';
 import { FechaComponent } from './fecha.component';
@@ -7,7 +8,7 @@ import { PieComponent } from './pie/pie.component';
 
 @NgModule({
   declarations: [CabeceraComponent, PieComponent, DesarrolladorComponent, FechaComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [CabeceraComponent, PieComponent],
 })
 export class LayoutModule {}
