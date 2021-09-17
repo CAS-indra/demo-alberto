98607f73a7b2c8aef507f9ddb2766f66f13e023b
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-17 11:01:20 +0200

    plantillas y componentes angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public proyectos = [
+    {
+      name: 'Aprender angular',
+      status: 1,
+    },
+    {
+      name: 'Tener un buen trabajo',
+      status: 0,
+    },
+  ];
+  public color = 'brown';
+
+  public elValor = 5;
+
+  // private paella = 55;
+  // public get elValor(): number {
+  //   return this.paella;
+  // }
+  // public set elValor(arg: number) {
+  //   this.paella = arg;
+  // }
+
+  public multiplo(cantidad: number): number {
+    return cantidad * this.elValor;
+  }
+
+  public incrementar(): void {
+    this.elValor++;
+  }
+  reducir(): void {
+    this.elValor--;
+  }
+}
